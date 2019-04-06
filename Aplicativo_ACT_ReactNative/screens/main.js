53162081b27e63aa655a08936444abf6a9c76368
Alex Xavier <ajxv20@gmail.com>
2019-04-06 14:33:16 -0300

    Add screen main.js

---
+++ Aplicativo_ACT_ReactNative/screens/main.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+
+export default class main {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#7d253b"
+        />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "row",
+      flexWrap: "wrap",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#FFF",
+      padding: 20
+    },
+    box: {
+      height: 135,
+      width: 135,
+      backgroundColor: "#FFF",
+      borderColor: "#7d253b",
+      borderWidth: 0.5,
+      margin: 10
+    }
+});
