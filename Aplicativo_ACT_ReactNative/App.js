69e31933042decf4f2087cbe7ad35ccf59dc7a96
Alex Xavier <ajxv20@gmail.com>
2019-04-06 14:32:01 -0300

    Add Code of Refactoring and ReactNavigation

--- Aplicativo_ACT_ReactNative/App.js
+++ Aplicativo_ACT_ReactNative/App.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer , createStackNavigator} from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+import main from "./screens/main";
+
+const AppNavigator = createStackNavigator ({
+  "main": {
+    screen: main
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
