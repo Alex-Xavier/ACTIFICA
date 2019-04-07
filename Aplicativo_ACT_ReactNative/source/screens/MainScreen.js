32f15990345b792a7c64a859e04e0ecf0d81554b
Alex Xavier <ajxv20@gmail.com>
2019-04-06 23:47:52 -0300

    Add Images and Tochs and stylezation in MainScreen

--- Aplicativo_ACT_ReactNative/source/screens/MainScreen.js
+++ Aplicativo_ACT_ReactNative/source/screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, StyleSheet, View } from 'react-native';
+import { StatusBar, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
 
 export default class MainScreen extends React.Component {
   render() {
@@ -9,33 +9,46 @@ export default class MainScreen extends React.Component {
           barStyle="light-content"
           backgroundColor="#7d253b"
         />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
+        <TouchableOpacity>
+          <Image style={styles.cards} source={require('../images/EuQuero.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.cards} source={require('../images/NaoQuero.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.cards} source={require('../images/EuEstou.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.cards} source={require('../images/NaoEstou.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.cards} source={require('../images/Escolher.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.cards} source={require('../images/Acoes.png')} />
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: "row",
-      flexWrap: "wrap",
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#FFF",
-      padding: 20
-    },
-    box: {
-      height: 135,
-      width: 135,
-      backgroundColor: "#FFF",
-      borderColor: "#7d253b",
-      borderWidth: 0.5,
-      margin: 10
-    }
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFF",
+    padding: 20
+  },
+  cards: {
+    aspectRatio: 1,
+    height: 135,
+    width: 135,
+    backgroundColor: "#FFF",
+    borderColor: "#7d253b",
+    borderWidth: 0.5,
+    margin: 10
+  }
 });
