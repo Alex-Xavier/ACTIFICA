c32fa04d562bade60ab8fccda4c24ef6b4441176
Alex Xavier <ajxv20@gmail.com>
2019-04-06 23:45:27 -0300

    Add CustomCardsScreen in Navigation

--- Aplicativo_ACT_ReactNative/App.js
+++ Aplicativo_ACT_ReactNative/App.js
@@ -1,10 +1,13 @@
 import {createAppContainer , createStackNavigator} from 'react-navigation';
-
 import MainScreen from './source/screens/MainScreen';
+import CustomCards from './source/screens/CustomCardsScreen';
 
 const AppNavigator = createStackNavigator ({
   'Main': {
     screen: MainScreen
+  },
+  'CustomCards': {
+    screen: CustomCards
   }
 });
 
