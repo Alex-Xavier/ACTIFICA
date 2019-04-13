1a6f8ec9c64c2edc9b1dda5645fd7ea7fd0ae455
Alex Xavier <ajxv20@gmail.com>
2019-04-13 12:59:14 -0300

    Tentando Ajustar base do Layout Card's x Button's

--- Aplicativo_ACT_ReactNative/source/screens/MainScreen.js
+++ Aplicativo_ACT_ReactNative/source/screens/MainScreen.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { StatusBar, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
-
-//Responsividade em Layout
-//var { height, width } = Dimension.get('window');
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  Dimensions
+} from 'react-native';
 
 export default class MainScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar
-          barStyle='light-content'
-          backgroundColor='#FFF'
-        />
-        <TouchableOpacity onPress={() => {
-          this.props.navigation.navigate('CustomCards');
-        }}>
-          <Image style={styles.cards} source={require('../images/mainscreen/EuQuero.png')} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.props.navigation.navigate('CustomCards');
-        }}>
-          <Image style={styles.cards} source={require('../images/mainscreen/NaoQuero.png')} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
+
+        <View style={styles.primaryContainer}>
+          <TouchableOpacity onPress={() => {
+            this.props.navigation.navigate('CustomCards');
+          }}>
+            <Image style={styles.cards} source={require('../images/mainscreen/EuQuero.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            this.props.navigation.navigate('CustomCards');
+          }}>
+            <Image style={styles.cards} source={require('../images/mainscreen/NaoQuero.png')} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.primaryLineContainer}>  
+          <TouchableOpacity style={styles.button} onPress={null}>
+            <Text style={styles.buttonText}>Eu Quero</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={null}>
+            <Text style={styles.buttonText}>Não Quero</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/*<TouchableOpacity onPress={() => {
           this.props.navigation.navigate('CustomCards');
         }}>
           <Image style={styles.cards} source={require('../images/mainscreen/EuEstou.png')} />
@@ -41,7 +54,7 @@ export default class MainScreen extends React.Component {
           this.props.navigation.navigate('CustomCards');
         }}>
           <Image style={styles.cards} source={require('../images/mainscreen/Acoes.png')} />
-        </TouchableOpacity>
+        </TouchableOpacity>*/}
       </View>
     );
   }
@@ -49,6 +62,53 @@ export default class MainScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  primaryContainer: {
+    height: Dimensions.get('window').width * 0.4,
+    width: Dimensions.get('window').height * 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+    backgroundColor: '#FFF'
+  },
+  primaryLineContainer: {
+    height: Dimensions.get('window').width * 0.15,
+    width: Dimensions.get('window').height * 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+    backgroundColor: '#FFF'
+  },
+  secondContainer: {
+    flex: 3,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    padding: 20
+  },
+  secondLineContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    padding: 20
+  },
+  thirdContainer: {
+    flex: 3,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    padding: 20
+  },
+  thirdLineContainer: {
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -65,5 +125,17 @@ const styles = StyleSheet.create({
     borderColor: '#7d253b',
     borderWidth: 0.5,
     margin: 10
+  },
+  button: {
+    height: 45,
+    backgroundColor: '#7d253b',
+    alignSelf: 'stretch',
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold'
   }
 });
