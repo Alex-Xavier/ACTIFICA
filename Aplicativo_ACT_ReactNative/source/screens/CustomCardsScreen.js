b3acbacce32ce9495c27dd434689c3e51973e723
Alex Xavier <ajxv20@gmail.com>
2019-04-20 20:47:52 -0300

    Excluindo código inútil

--- Aplicativo_ACT_ReactNative/source/screens/CustomCardsScreen.js
+++ Aplicativo_ACT_ReactNative/source/screens/CustomCardsScreen.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { StatusBar, StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default class CustomCardsScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar
-          barStyle='light-content'
-          backgroundColor='#FFF'
-        />
+        
         <Text>Eu sou a CustomCardsScreen!!</Text>
+      
       </View>
     );
   }
@@ -18,9 +16,7 @@ export default class CustomCardsScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      flexWrap: 'wrap',
       alignItems: 'center',
-      backgroundColor: '#FFF',
-      padding: 20
+      backgroundColor: '#FFF'
     }
 });
