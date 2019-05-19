62bd5f456d27940b419d755646d98d5df3ef44bd
Alex Xavier <ajxv20@gmail.com>
2019-05-19 15:29:54 -0300

    Exclusão do  Card Quer

--- Aplicativo_ACT_ReactNative/source/screens/AfterPeopleVerbsScreen.js
+++ Aplicativo_ACT_ReactNative/source/screens/AfterPeopleVerbsScreen.js
@@ -13,47 +13,29 @@ export default class AfterPeopleVerbsScreen extends React.Component {
     return (
       <View style={styles.container}>
 
-        <View style={styles.cardContainer}>
           <TouchableOpacity onPress={() => {
             this.props.navigation.navigate('PeopleFeatures1');
           }}>
             <Image style={styles.cards} source={require('../images/afterpeopleverbsscreen/esta.png')} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => {
-            this.props.navigation.navigate('SecondMain');
-          }}>
-            <Image style={styles.cards} source={require('../images/mainscreen1/euquero.png')} />
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.buttonContainer}>  
+  
           <TouchableOpacity style={styles.button} onPress={() => {
             this.props.navigation.navigate('PeopleFeatures1');
           }}>
             <Text style={styles.buttonText}>Está</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => {
-            this.props.navigation.navigate('SecondMain');
-          }}>
-            <Text style={styles.buttonText}>Quer</Text>
-          </TouchableOpacity>
-        </View>
 
-        <View style={styles.cardContainer}>
           <TouchableOpacity onPress={() => {
             this.props.navigation.navigate('PeopleFeatures1');
           }}>
             <Image style={styles.cards} source={require('../images/afterpeopleverbsscreen/e.png')} />
           </TouchableOpacity>
-        </View>
 
-        <View style={styles.buttonContainer}>  
           <TouchableOpacity style={styles.button} onPress={() => {
             this.props.navigation.navigate('PeopleFeatures1');
           }}>
             <Text style={styles.buttonText}>é</Text>
           </TouchableOpacity>
-        </View>
 
       </View>
     );
@@ -63,44 +45,31 @@ export default class AfterPeopleVerbsScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#FFF',
-  },
-  cardContainer: {
-    height: Dimensions.get('window').width * 0.42,
-    width: Dimensions.get('window').height * 1,
-    flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: Dimensions.get('window').height * 0.04,
-    backgroundColor: '#FFF'
-  },
-  buttonContainer: {
-    height: Dimensions.get('window').width * 0.09,
-    width: Dimensions.get('window').height * 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: Dimensions.get('window').height * 0.04,
-    backgroundColor: '#FFF'
+    justifyContent: 'center'
   },
   cards: {
     aspectRatio: 1,
     resizeMode: 'contain',
-    height: Dimensions.get('window').width * 0.367,
-    width: Dimensions.get('window').height * 0.367,
+    height: Dimensions.get('window').width * 0.58,
+    width: Dimensions.get('window').height * 0.58,
     backgroundColor: '#FFF',
     borderColor: '#7d253b',
     borderWidth: 0.5,
-    margin: Dimensions.get('window').height * 0.018
+    margin: Dimensions.get('window').height * 0.01
   },
   button: {
-    height: Dimensions.get('window').width * 0.08,
-    width: Dimensions.get('window').height * 0.209,
+    height: Dimensions.get('window').width * 0.09,
+    width: Dimensions.get('window').height * 0.3265,
     backgroundColor: '#7d253b',
     justifyContent: 'center',
     alignItems: 'center',
     borderColor: '#7d253b',
     borderWidth: 0.5,
     borderRadius: 5,
-    margin: Dimensions.get('window').height * 0.017
+    margin: Dimensions.get('window').height * 0.01
   },
   buttonText: {
     color: '#FFF',
