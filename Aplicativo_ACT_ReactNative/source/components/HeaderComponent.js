5dafbe55d06f9adee485a134bbb568fc6e622422
Alex Xavier <ajxv20@gmail.com>
2019-08-31 13:22:39 -0300

    Estilizando header

--- Aplicativo_ACT_ReactNative/source/components/HeaderComponent.js
+++ Aplicativo_ACT_ReactNative/source/components/HeaderComponent.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Header, Left, Body, Right, Title } from 'native-base';
+import { StyleSheet } from 'react-native';
 
 export default class HeaderComponent extends React.Component {
   render() {
     return (
-      <Header>
+      <Header androidStatusBarColor='#850002' style={styles.header}>
         <Left/>
         <Body>
-          <Title>Header</Title>
+          <Title style={styles.title}>Sobre o Aplicativo</Title>
         </Body>
         <Right/>
       </Header>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#b80003'
+  },
+  title: {
+    fontWeight: 'bold'
+  }
+})
